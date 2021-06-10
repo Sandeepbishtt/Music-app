ed4dfbe715a321b8255427ff59e0463ccee4f851
Sandeep Bisht <82570357+Sandeepbishtt@users.noreply.github.com>
2021-06-10 17:31:14 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data=[
+    {
+    title: "Song 1",
+        artist: "Buddha",
+        img_src: "./images/1.jpg",
+        src: "./music/1.mp3"
+  }
+  ,
+    {title: "Song 2",
+        artist: "Buddha2",
+        img_src: "./images/2.jpg",
+        src: "./music/2.mp3"
+  }
+  ,
+   { title: "Song 3",
+        artist: "Buddha3",
+        img_src: "./images/3.jpg",
+        src: "./music/3.mp3"
+  }
+  ,
+    {title: "Song 4",
+        artist: "Buddha4",
+        img_src: "./images/4.jpg",
+        src: "./music/4.mp3"
+  }
+  ,
+    {title: "Song 5",
+        artist: "Buddha5",
+        img_src: "./images/5.jpg",
+        src: "./music/5.mp3"
+  }
+  ,
+    {title: "Song 6",
+        artist: "Buddha6",
+        img_src: "./images/6.jpg",
+        src: "./music/6.mp3"
+  }
+  ,
+    {title: "Song 7",
+        artist: "Buddha7",
+        img_src: "./images/7.jpg",
+        src: "./music/7.mp3"
+  }
+  ,
+    {title: "Song 8",
+        artist: "Buddha8",
+        img_src: "./images/8.jpg",
+        src: "./music/8.mp3"
+  }
+  ,
+    {title: "Song 9",
+        artist: "Buddha9",
+        img_src: "./images/9.jpg",
+        src: "./music/9.mp3"
+  }
+  , 
+  {title: "Song 10",
+  artist: "Buddha10",
+  img_src: "./images/10.jpg",
+  src: "./music/10.mp3"
+}
+  ]
+
+  export default data
